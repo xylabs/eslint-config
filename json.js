380e754363b7f85b4ef29be2920c5054d34cac7f
Arie Trouw <arie.trouw@xylabs.com>
2021-11-24 14:09:23 -0800

    Switch to JSON files

--- json.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "overrides": [{
-        "files": ["*.json"],
-        "parser": "eslint-plugin-json-es",
-        "extends": "plugin:eslint-plugin-json-es/recommended",
-        "rules": {
-        }
-    }]
-}
