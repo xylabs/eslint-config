9e50f39763080f91a1c5f887cfb434894cbcf53f
Arie Trouw <arie.trouw@xylabs.com>
2022-01-06 13:35:45 -0800

    paths

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import '@rushstack/eslint-patch'
+// eslint-disable-next-line import/no-internal-modules
+import '@rushstack/eslint-patch/modern-module-resolution'
+
+import { cwd } from 'process'
 
 export = {
   extends: ['./typescript', './prettier', './import', './rules', './json', './markdown'],
   ignorePatterns: ['dist/**/*.*', 'build/**/*.*', 'bin/**/*.*', 'node_modules/**/*.*'],
-  parserOptions: { tsconfigRootDir: __dirname },
+  parserOptions: { tsconfigRootDir: cwd() },
 }
