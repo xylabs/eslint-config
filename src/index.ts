94d92f356f1468f8ac271200561a91ac589af67d
Arie Trouw <arie.trouw@xylabs.com>
2022-01-06 13:16:11 -0800

    added eslint-patch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import '@rushstack/eslint-patch'
+
 export = {
   extends: ['./typescript', './prettier', './import', './rules', './json', './markdown'],
   ignorePatterns: ['dist/**/*.*', 'build/**/*.*', 'bin/**/*.*', 'node_modules/**/*.*'],
+  parserOptions: { tsconfigRootDir: __dirname },
 }
