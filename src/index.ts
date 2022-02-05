e4ebc1214ee6aa160458ae3e624b0915eeb01119
Arie Trouw <arie.trouw@xylabs.com>
2022-02-04 16:10:48 -0800

    markdown removed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import '@rushstack/eslint-patch/modern-module-resolution'
 import { cwd } from 'process'
 
 export = {
-  extends: ['./typescript', './prettier', './import', './rules', './json', './markdown'],
+  extends: ['./typescript', './prettier', './import', './rules', './json'],
   ignorePatterns: ['dist/**/*.*', 'build/**/*.*', 'bin/**/*.*', 'node_modules/**/*.*', 'package.json'],
   parserOptions: { tsconfigRootDir: cwd() },
 }
