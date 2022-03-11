8252472ae506efe56ba0021ec9eadd93486ae85a
Arie Trouw <arie.trouw@xylabs.com>
2022-03-11 15:58:34 -0800

    moved ts config out of overrides

--- src/import/index.ts
+++ src/import/index.ts
@@ -1,35 +1,30 @@
 export = {
   extends: ['plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
-      rules: {
-        'import/namespace': ['warn'],
-        'import/no-absolute-path': ['warn'],
-        'import/no-cycle': [
-          'off',
-          {
-            maxDepth: 2,
-          },
-        ],
-        'import/no-default-export': ['warn'],
-        'import/no-deprecated': ['warn'],
-        'import/no-internal-modules': [
-          'warn',
-          {
-            allow: ['lodash/*'],
-          },
-        ],
-        'import/no-named-as-default': ['warn'],
-        'import/no-restricted-paths': ['warn'],
-        'import/no-self-import': ['warn'],
-        'import/no-useless-path-segments': ['warn'],
-        'simple-import-sort/exports': ['warn'],
-        'simple-import-sort/imports': ['warn'],
-      },
-    },
-  ],
   plugins: ['import', 'simple-import-sort'],
+  rules: {
+    'import/namespace': ['warn'],
+    'import/no-absolute-path': ['warn'],
+    'import/no-cycle': [
+      'off',
+      {
+        maxDepth: 2,
+      },
+    ],
+    'import/no-default-export': ['warn'],
+    'import/no-deprecated': ['warn'],
+    'import/no-internal-modules': [
+      'warn',
+      {
+        allow: ['lodash/*'],
+      },
+    ],
+    'import/no-named-as-default': ['warn'],
+    'import/no-restricted-paths': ['warn'],
+    'import/no-self-import': ['warn'],
+    'import/no-useless-path-segments': ['warn'],
+    'simple-import-sort/exports': ['warn'],
+    'simple-import-sort/imports': ['warn'],
+  },
   settings: {
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
