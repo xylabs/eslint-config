ac2dfac1fa14d101485b72c73c5c6a40bb7bd04e
Arie Trouw <arie.trouw@xylabs.com>
2022-06-21 17:25:38 -0700

    markdown/deploy

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import '@rushstack/eslint-patch/modern-module-resolution'
 import { cwd } from 'process'
 
 export = {
-  extends: ['./typescript', './prettier', './import', './rules', './json'],
+  extends: ['./typescript', './prettier', './import', './rules', './json', './markdown'],
   ignorePatterns: ['/dist', '/build', '/bin', '/node_modules', 'package.json', '/.yarn'],
   parserOptions: { tsconfigRootDir: cwd() },
 }
